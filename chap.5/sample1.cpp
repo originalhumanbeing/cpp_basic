ca84f40732d6fea9d472ae78433c562ca6e5bfb5
originalhumanbeing <han.hyunjung17@gmail.com>
2016-09-30 21:59:46 +0900

    forth commit

---
+++ chap.5/sample1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int res;
+	cout << "정수를 입력하십시오. \n";
+
+	cin >> res;
+
+	if(res==1)
+	{
+		cout << "1이 입력되었습니다. \n";
+	}
+	
+	cout << "처리를 종료합니다. \n";
+
+	return 0;
+}
